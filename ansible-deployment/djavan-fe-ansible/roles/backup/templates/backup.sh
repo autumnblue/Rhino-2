cf2945cdd2b8e7e1498c970aee7fe9ce286f97ac
robert <margeson@expertnetwork-llc.com>
2017-03-09 15:30:01 -0500

    syncing todays changes

--- ansible-deployment/djavan-fe-ansible/roles/backup/templates/backup.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-
-NOW=`date +"%Y%m%d%H%M"`
-pg_dump  -Fc "postgresql://{{ database.user }}:{{ database.password }}@127.0.0.1/{{ database.name }}" -f {{ project.root }}/backup/djavan_$NOW.pg
