e65aefdb36671572ac6abfeea2fb189e36bf01fc
hiroki <hiroki.moto.pro@outlook.com>
2017-05-01 03:46:42 +0200

    Fix login component

--- shared/components/Login/index.js
+++ shared/components/Login/index.js
@@ -1,7 +1,47 @@
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import Login from './Login';
+import LoginComponent from './Login';
 import SessionActions from '../../actions/sessions';
 import LoginActions from '../../actions/login';
+
+class Login extends Component {
+  state = {
+    email: null,
+    password: null,
+  }
+
+  handleEmailChange = (value) => {
+    this.setState({ email: value });
+  }
+
+  handlePasswordChange = (value) => {
+    this.setState({ password: value });
+  }
+
+  handleRememberChange = (value) => {
+    console.log(value);
+  }
+
+  handleSubmit = () => {
+    const { email, password } = this.state;
+    const data = { email: email, passowrd: password };
+    this.props.userLoginRequest(data);
+  }
+
+  render() {
+    return (
+      <LoginComponent
+        title="Login"
+        backLink="/"
+        valid={false}
+        handleEmailChange={this.handleEmailChange}
+        handlePasswordChange={this.handlePasswordChange}
+        handleRememberChange={this.handleRememberChange}
+        handleSubmit={this.handleSubmit}
+      />
+    );
+  }
+}
 
 const mapStateToProps = ({session, login}) => ({
   email: session.email,
@@ -14,7 +54,7 @@ const mapStateToProps = ({session, login}) => ({
 const mapDispatchToProps = (dispatch) => ({
   validPassword: () => dispatch(LoginActions.validPassword()),
   validEmail: () => dispatch(LoginActions.validEmail()),
-  onLogin: (data) => dispatch(SessionActions.login(data)),
+  userLoginRequest: (data) => dispatch(SessionActions.userLoginRequest(data)),
   resetSession: () => dispatch(SessionActions.resetSession()),
   setErrorMessage: (errors) => dispatch(SessionActions.setErrorMessage(errors)),
 });
