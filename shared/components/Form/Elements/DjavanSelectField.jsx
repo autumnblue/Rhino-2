511c084540e0dcb98510cb38c9ee8c1c7c5e6ab7
hiroki <hiroki.moto.pro@outlook.com>
2017-05-02 17:33:14 +0900

    Fix DjavanSelectField

--- shared/components/Form/Elements/DjavanSelectField.jsx
+++ shared/components/Form/Elements/DjavanSelectField.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import makeBem from 'bem-cx';
 import SelectField from 'material-ui/SelectField';
 import { djavanTheme, borderGrey, errorRed } from '../../../constants/djavanTheme';
