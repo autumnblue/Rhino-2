b73252df992e73767d70b99aebfc651d2c4947ab
hiroki <hiroki.moto.pro@outlook.com>
2017-05-02 17:29:15 +0900

    Fix DjavanCheckboxGroupView

--- shared/components/Form/Elements/DjavanCheckboxGroupView.jsx
+++ shared/components/Form/Elements/DjavanCheckboxGroupView.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import makeBem from 'bem-cx';
 
 const bem = makeBem('djavanFormElement');
