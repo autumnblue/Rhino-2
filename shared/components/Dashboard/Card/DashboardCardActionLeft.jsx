153873d2233a5ceacd6264983db9a61190f6d978
hiroki <hiroki.moto.pro@outlook.com>
2017-05-02 16:29:38 +0900

    Fix DashboardCardActionLeft

--- shared/components/Dashboard/Card/DashboardCardActionLeft.jsx
+++ shared/components/Dashboard/Card/DashboardCardActionLeft.jsx
@@ -1,8 +1,8 @@
-import React, { PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import makeBem from 'bem-cx';
 
 const bem = makeBem('djavanDashboardCard');
-
 
 const DashboardCardActionLeft = ({ children }) =>
   <div className={bem.el('action').mod({ left: true })}>
