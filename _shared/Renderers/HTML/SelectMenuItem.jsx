757cbebc963446a9dba21f4ea9ff3b360c063ecb
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2017-06-30 20:51:35 +0300

    Cleanup

--- _shared/Renderers/HTML/SelectMenuItem.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import _ from 'lodash';
-import MenuItem from 'material-ui/MenuItem';
-
-const renderList = (data) =>
-  _.map(data, (value, index) => <MenuItem value={value.id || value.value || value.name} key={index} primaryText={value.name || value.value} />);
-
-export default renderList;
