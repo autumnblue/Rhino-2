37f40e576bbe5136ec7642a950ca1124c338f8ef
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2017-07-03 00:48:22 +0300

    Add missing file to git index

---
+++ src/helpers/simpleObjectDiff.js
@@ -0,0 +1,11 @@
+export default function simpleObjectDiff(source, target) {
+  const result = {};
+
+  for(const [key, value] of Object.entries(source)) {
+    if(value !== target[key]) {
+      result[key] = value;
+    }
+  }
+
+  return result;
+}
